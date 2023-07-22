597926b8d3518878d07c61b8e46d5fe4875d26be
mauricio-delgado <mauri.delgadom2001@gmail.com>
2023-07-22 11:37:03 -0300

    agregamos api

--- index.js
+++ index.js
@@ -3,12 +3,25 @@ const listaCumples = document.getElementById("listaCumples");
 
 const cumpleanos = JSON.parse(localStorage.getItem("cumpleanos")) || [];
 
-function mostrarCumples() {
+async function mostrarCumples() {
   listaCumples.innerHTML = "";
 
-  cumpleanos.forEach((cumple, index) => {
+  for (const [index, cumple] of cumpleanos.entries()) {
     const li = document.createElement("li");
-    li.textContent = `${cumple.nombre} - ${cumple.fecha}`;
+
+    const mesCumple = obtenerMes(cumple.fecha);
+    const rutaImagen = await obtenerImagenMes(mesCumple);
+    if (rutaImagen) {
+      const img = document.createElement("img");
+      img.src = rutaImagen;
+      img.alt = mesCumple;
+      img.classList.add("imgCumple");
+      li.appendChild(img);
+    }
+
+    const nombreFechaTexto = document.createElement("span");
+    nombreFechaTexto.textContent = `${cumple.nombre} - ${cumple.fecha}`;
+    li.appendChild(nombreFechaTexto);
 
     const deleteButton = document.createElement("button");
     deleteButton.textContent = "Eliminar";
@@ -20,7 +33,38 @@ function mostrarCumples() {
 
     li.appendChild(deleteButton);
     listaCumples.appendChild(li);
-  });
+  }
+}
+
+function obtenerMes(fecha) {
+  const fechaObj = new Date(fecha);
+  const meses = [
+    "enero",
+    "febrero",
+    "marzo",
+    "abril",
+    "mayo",
+    "junio",
+    "julio",
+    "agosto",
+    "septiembre",
+    "octubre",
+    "noviembre",
+    "diciembre",
+  ];
+  return meses[fechaObj.getMonth()];
+}
+
+async function obtenerImagenMes(mes) {
+  try {
+    const response = await fetch("api.json");
+    const data = await response.json();
+    const mesData = data.find((item) => item.nombre === mes);
+    return mesData ? mesData.imagen : null;
+  } catch (error) {
+    console.error("Error al cargar el archivo 'api.json':", error);
+    return null;
+  }
 }
 
 function loGuardo() {
